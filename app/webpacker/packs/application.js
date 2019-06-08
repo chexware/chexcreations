ea0540bfafa876113c99e9d20ff1f20010b1df9d
Aleksandar Brayanov <aleksandar.brayanov@hotmail.com>
2019-06-08 16:29:41 +0100

    stylesheets migrated to webpack

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -17,8 +17,8 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
-require("trestle/custom")
+require("src/javascript/channels")
+require("src/javascript/trestle/custom")
 import 'bootstrap/dist/js/bootstrap';
 $(function () {
     console.log('Hello World from Webpacker');
