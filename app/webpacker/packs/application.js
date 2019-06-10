4f175461c727540faf28055bbe96a5779ae901f5
Aleksandar Brayanov <aleksandar.brayanov@hotmail.com>
2019-06-10 20:44:13 +0100

    design update started; migrating to UIkit

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -12,14 +12,20 @@
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// const images = require.context('../images', true)
+const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("src/javascript/channels")
 require("src/javascript/trestle/custom")
+
+
 import 'bootstrap/dist/js/bootstrap';
-$(function () {
-    console.log('Hello World from Webpacker');
-});
+import UIkit from 'uikit';
+import Icons from 'uikit/dist/js/uikit-icons';
+// loads the Icon plugin
+UIkit.use(Icons);
+
+// components can be called from the imported UIkit reference
+UIkit.notification('Hello world.');
