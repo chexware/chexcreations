8e7eaf4c25fc936e0dacb5d1d3da076ec8848286
Aleksandar Brayanov <aleksandar.brayanov@hotmail.com>
2019-10-10 00:51:06 +0100

    logo should appear

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -12,10 +12,12 @@
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
-// /const images = require.context('../images', true)
+
 // const imagePath = (name) => images(name, true)
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("src/javascript/channels")
 require("src/javascript/trestle/custom")
+require.context('../images', true)
+image_pack_path('default.jpg')
