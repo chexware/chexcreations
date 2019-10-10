b0b3c02430b2510bf70326d3bfa6a6a744ee6adc
Aleksandar Brayanov <aleksandar.brayanov@hotmail.com>
2019-10-10 03:34:03 +0100

    updated sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,10 +69,10 @@ Rails.application.configure do
 
    config.action_mailer.default_url_options = { host: 'https://chexcreations.herokuapp.com' }
    config.action_mailer.delivery_method = :smtp
-   config.action_mailer.perform_deliveries = true
-   config.action_mailer.raise_delivery_errors = true
-   config.action_mailer.default :charset => "utf-8"
-   config.action_mailer.perform_caching = true
+   #config.action_mailer.perform_deliveries = true
+   #config.action_mailer.raise_delivery_errors = true
+   #config.action_mailer.default :charset => "utf-8"
+   #config.action_mailer.perform_caching = true
    ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => 587,
