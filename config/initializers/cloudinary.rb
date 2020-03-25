15aae44a099efd02852d2b232de5899b8161567c
Aleksandar  Brayanov <aleksandar@chexvisual.co.uk>
2020-03-25 03:42:47 +0000

    Delete cloudinary.rb

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-    config.cloud_name = 'hoppueffz'
-    config.api_key = '563347888264553'
-    config.api_secret = 'NGGYodIhVVSSMpAFUZyK6fvPDek'
-    config.secure = true
-    config.cdn_subdomain = true
-  end
