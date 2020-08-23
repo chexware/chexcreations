9a7a3440db6f3afc795efc8a8ab2750400062c69
Aleksandar Brayanov <aleksandar@chexvisual.co.uk>
2020-08-23 06:28:58 +0100

    layout update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :contacts, only: [:new, :create]
   root to: 'home#index'
   get 'art', to: 'project#load_art'
-  get 'software', to: 'project#load_software'
+  get 'blog', to: 'project#load_software'
   get '404', to: 'exceptions#not_found'
   get '422', to: 'exceptions#unacceptable'
   get '500', to: 'exceptions#internal_error'
