6124151729b96dd3fd7d98c0b12255854e4190e2
Aleksandar Brayanov <aleksandar@chexvisual.co.uk>
2020-08-24 10:02:03 +0100

    rails 6 configurations added

--- config/initializers/sendgid.rb
+++
@@ -1,7 +0,0 @@
-Sendgrid.config do |config|
-    config.cloud_name = 'hoppueffz'
-    config.api_key = 'SG.R0pwZzG2TaqQaNJ0NjLRBw.Uw1AGMHb2X5edqpVej4CASO0sBDBZKPtz2NC8H-Y3l0Copied!'
-    config.api_secret = 'NGGYodIhVVSSMpAFUZyK6fvPDek'
-    config.secure = true
-    config.cdn_subdomain = true
-end
