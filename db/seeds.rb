d82af93f689feb32b4da4db650ef49fa8a8123c3
Aleksandar Brayanov <aleksandar@chexvisual.co.uk>
2020-08-24 07:08:35 +0100

    update project structure

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'date'
 
-thought_content = "Test " * 300
-
-Tag.destroy_all
-Tag.create!(name: 'Music')
-Tag.create!(name: 'Software')
-Tag.create!(name: 'Art')
-p "Created #{Tag.count} tags"
-
-Thought.destroy_all
-Thought.create!(title: 'Topic Test 1',publish_date: DateTime.parse('2016-05-19 10:30:14'), content: thought_content)
-Thought.create!(title: 'Topic Test 2',publish_date: DateTime.parse('2016-05-19 10:30:15'), content: thought_content)
-Thought.create!(title: 'Topic Test 3',publish_date: DateTime.parse('2016-05-19 10:30:16'), content: thought_content)
-Thought.create!(title: 'Topic Test 4',publish_date: DateTime.parse('2016-05-19 10:30:17'), content: thought_content)
-p "Created #{Thought.count} thoughts"
-
-User.destroy_all
-User.create!(name: 'admin',password_digest: 'pass')
-p "Created admin user"
-
 
 
