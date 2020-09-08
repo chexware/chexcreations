107072abbf941b01ea941902b56cc7ec209e362c
Aleksandar Brayanov <aleksandar@chexvisual.co.uk>
2020-09-08 04:39:46 +0100

    fix failed biuld

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,8 +29,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
-  config.assets.digest = true
+  #config.assets.compile = true
+  #config.assets.digest = true
 
   config.active_storage.service = :cloudinary
 
