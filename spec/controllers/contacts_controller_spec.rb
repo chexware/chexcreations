0bc2786631c2b21bcff86e6549980efd2e921986
Aleksandar Brayanov <aleksandar@chexvisual.co.uk>
2020-09-13 07:30:17 +0100

    updated controllers test coverage

--- spec/controllers/contacts_controller_spec.rb
+++ spec/controllers/contacts_controller_spec.rb
@@ -13,24 +13,35 @@ RSpec.describe ContactsController, type: :controller do
               post :create, session: { name: "Test Name", email: "test-email.com", message: "test message" }
               expect(response).to redirect_to "/contact"
             end
-          end
-        
-          context "when form is valid" do
-            it "redirects to the index page page" do
-              post :create, session: { name: "Test Name", email: "test@email.com", message: "test message" }
-        
-              expect(response).to redirect_to "/contact"
-            end
 
             it "get all projects" do
-                #contact = create(:contact, name: "Test Name", email: "test@email.com", message: "test message")
-                project = create(:project, title: "test", link: "http://link.test.com")
-                project2 = create(:project, title: "test2", link: "http://link.test2.com")
+              #contact = create(:contact, name: "Test Name", email: "test@email.com", message: "test message")
+              project = create(:project, title: "test", link: "http://link.test.com")
+              project2 = create(:project, title: "test2", link: "http://link.test2.com")
+      
+              post :create, session: { name: "Test Name", email: "test-email.com", message: "test message" }
         
-                post :create, session: { name: "Test Name", email: "test@email.com", message: "test message" }
-          
-                expect(assigns(:projects)).to eq([project,project2])
-              end
+              expect(assigns(:projects)).to eq([project,project2])
+            end
+      end
+         
+      context "when form is valid" do
+        it "redirects to the contacts#new page" do
+          post :create, session: { name: "Test Name", email: "test@email.com", message: "test message" }
+    
+          expect(response).to redirect_to "/contact"
+        end
+        
+
+        it "get all projects" do
+            #contact = create(:contact, name: "Test Name", email: "test@email.com", message: "test message")
+            project = create(:project, title: "test", link: "http://link.test.com")
+            project2 = create(:project, title: "test2", link: "http://link.test2.com")
+    
+            post :create, session: { name: "Test Name", email: "test@email.com", message: "test message" }
+      
+            expect(assigns(:projects)).to eq([project,project2])
           end
+      end
     end
 end
