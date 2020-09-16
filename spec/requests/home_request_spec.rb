b613bb60a974ac77cb1fd94a020a7c984b2dc938
Aleksandar Brayanov <aleksandar@chexvisual.co.uk>
2020-09-16 01:57:15 +0100

    added request tests

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -4,8 +4,15 @@ RSpec.describe 'Home', type: :request do
     describe 'GET index' do
         it 'renders home page' do
             get('/')
+            expect(response).to have_http_status(:ok)
             expect(response).to render_template('home/index')
+            expect(response).to render_template('project/_load')
+            expect(response).to render_template('layouts/_about')
+        end
+
+        it 'does not render a different template' do
+            get('/')
+            expect(response).to_not render_template('home/show')
         end
     end
-    pending "TO DO: Home page tests"
 end
